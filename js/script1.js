8f624a8a6ea528f5650d724930c0b9648e6de446
Ekaterina Matveeva <>
2021-02-10 12:30:38 +0300

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,3 +1,10 @@
-/*let hello;
-hello='Привет';*/
-alert('Hello world!')
+/*Урок 1
+let hello;
+hello='Привет';
+alert('Hello world!')*/
+
+//Урок 2
+let num;
+let score;
+const question1 = 0;
+let question2, question3, question4;
